0c10c1d39ec9a10dbae04f342bfea36059d690b6
Emily <emzlewis@hotmail.co.uk>
2021-06-07 16:08:56 +0100

    Merge changes

--- JET_EFIT.py
+++ JET_EFIT.py
@@ -6,7 +6,7 @@ try:
     import numpy
     import csv
     import pandas as pd
-    
+
     import py_flush as Flush
     from scipy import optimize
 
@@ -40,41 +40,49 @@ sys.path[:0] = ["/jet/share/lib/python"]
 from ppf import *
 
 # --- data functions
-class DATA:
+class DATA(EFIT_params):
     # Initialise global variables
     def __init__(self):  # MACHINE DEPENDENT
-        #DATA.pulse = 82631
+        # DATA.pulse = 82631
         DATA.pulse = pulse_number
         DATA.t_min = 60.5
         DATA.t_max = 62.0
-        DATA.i_min = 0
-        DATA.i_max = 0
         DATA.psi_shift = 0.0
-        DATA.x = numpy.array([])
-        DATA.y_ne = numpy.array([])
-        DATA.y_Te = numpy.array([])
-        DATA.y_psi = numpy.array([])
-        DATA.ne = numpy.array([])
-        DATA.Te = numpy.array([])
-        DATA.ne_coef = numpy.zeros(7)
-        DATA.Te_coef = numpy.zeros(7)
-        DATA.psi = numpy.array([])
-        DATA.NBI_t = numpy.array([])
-        DATA.NBI_ptot = numpy.array([])
+        DATA.params = EFIT_params
+
+    def load_params(self):
+        for param in EFIT_params:
+            print("I'm working")
+            print(params)
+        ## TODO add DATA.values for all the extra EFIT stuff we are adding
+
+    # https://stackoverflow.com/questions/32721580/example-of-class-with-user-input
+    """
+    @classmethod
+    def from_input(cls):
+        return cls(
+            raw_input('Name: '),
+            int(raw_input('User ID: ')), 
+            int(raw_input('Reputation: ')),
+        )
+    """
 
     # --- Load the pulse basic data # MACHINE DEPENDENT
-    def set_pulse(self, widget):
-        #DATA.pulse = int(DATA.pulse_box.get_text())
-        DATA.pulse = pulse_number
+    def set_pulse(self):
+
+        # where EFIT_params is a list of all the metrics we want to retrieve
+
+        # DATA.pulse = int(DATA.pulse_box.get_text())
+        DATA.pulse = 82630
 
         # --- Prepare JETPPF system (yes, you need to knock before you open it...) # MACHINE DEPENDENT
         ier = ppfgo(pulse=DATA.pulse, seq=0)
         ppfuid("JETPPF", rw="R")
         ier = ppfgo(pulse=DATA.pulse, seq=0)
 
-
         # --- Load EFIT data # MACHINE DEPENDENT
         dda = "EFIT"
+
         dtyp = "XIP"
         try:
             ihdat, iwdat, data, x, t, ier = ppfget(
@@ -83,24 +91,44 @@ class DATA:
         except:
             print("Failed to load EFIT data, pulse number may not exist")
         DATA.EFIT_t = t
+        DATA.x = x
         DATA.EFIT_xip = data
-        return(data,t)
+
+        # need to grab EFIT time once at beginning
+        """
+        for param in EFIT_params:
+            dtyp = str(param)
+            ihdat, iwdat, data, x, t, ier = ppfget(
+                DATA.pulse, dda, dtyp, fix0=0, reshape=0, no_x=0, no_t=0
+            )
+            if ier != 0:
+                self.pop_up_message(self, "failed to load EFIT/XIP data")
+                return
+            DATA.EFIT_xip = data
+        """
+
+        ###
+
+        ### next one
+
+        return (data, t)
 
 
 # Main function to run whole thing
 class Main:
     def __init__(self):
-        data_thread = DATA()
+        params_to_retrive = ["AREA", "BTPD"]
+        data_thread = DATA(params_to_retrive)
         retrieved_data, retrieved_time = data_thread.set_pulse(data_thread)
         print(len(retrieved_data))
         print(len(retrieved_time))
         print(retrieved_data[0:4])
         print(retrieved_time[0:4])
-        data_dict = {'efit':retrieved_data,'time':retrieved_time}
+        data_dict = {"efit": retrieved_data, "time": retrieved_time}
         df = pd.DataFrame(data_dict)
-        
-        filename = str(pulse_number) + '_EFIT.csv'
-        with open(filename,mode='w') as f:
+
+        filename = str(pulse_number) + "_EFIT.csv"
+        with open(filename, mode="w") as f:
             df.to_csv(f)
 
 
